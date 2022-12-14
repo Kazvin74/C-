dd602bd0a9fc0b270219ac70c8fc4716887f192e
Kazvin74 <120466779+Kazvin74@users.noreply.github.com>
2022-12-14 19:37:46 +0800

    Create All Operations Calculator.cpp

---
+++ All Operations Calculator.cpp
@@ -0,0 +1,32 @@
+// All Operations Calculator
+// Created by Kazvin
+// Coded in my phone
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    float FirstNumber;
+    float SecondNumber;
+    float OperationNumber = 1;
+    float Sum;
+    float Difference;
+    float Product;
+    float Quotient;
+    for ( ; ; ) {
+        cout << "Operation #" << OperationNumber << endl;
+        cout << "First Number: ";
+        cin >> FirstNumber;
+        cout << "Second Number: ";
+        cin >> SecondNumber;
+        Sum = FirstNumber + SecondNumber;
+        Difference = FirstNumber - SecondNumber;
+        Product = FirstNumber * SecondNumber;
+        Quotient = FirstNumber / SecondNumber;
+        cout << "Sum: " << Sum << endl;
+        cout << "Difference: " << Difference << endl;
+        cout << "Product: " << Product << endl;
+        cout << "Quotient: "<< Quotient << endl << endl;
+        OperationNumber++;
+    }
+}
