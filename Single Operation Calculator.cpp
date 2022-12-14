cf86344dfc55549cc5ca0492d1d2c212140be8a8
Kazvin74 <120466779+Kazvin74@users.noreply.github.com>
2022-12-14 19:33:55 +0800

    Create Single Operation Calculator.cpp

---
+++ Single Operation Calculator.cpp
@@ -0,0 +1,51 @@
+// Single Operation Calculator
+// Created by Kazvin
+// Coded in my phone
+
+#include <iostream>
+using namespace std;
+
+int main () {
+    float FirstNumber;
+    float SecondNumber;
+    float OperationNumber = 1;
+    char Operator;
+    float Answer;
+    for ( ; ; ) {
+        cout << "Operation #" << OperationNumber << endl;
+        cout << "First Number: ";
+        cin >> FirstNumber;
+        cout << "Second Number: ";
+        cin >> SecondNumber;
+        cout << "Operator (+, -, *, /): ";
+        cin >> Operator;
+
+        switch (Operator) {
+
+        case '+':
+             Answer = FirstNumber + SecondNumber;
+            cout << "Sum: " << Answer << endl << endl;
+            break;
+
+        case '-':
+            Answer = FirstNumber - SecondNumber;
+            cout << "Difference: " << Answer << endl << endl;
+            break;
+
+        case '*':
+            Answer = FirstNumber * SecondNumber;
+            cout << "Product: " << Answer << endl << endl;
+            break;
+
+        case '/':
+            Answer = FirstNumber / SecondNumber;
+            cout << "Quotient: " << Answer << endl << endl;
+            break;
+
+        default:
+            cout << "Invalid Operator" << endl << endl;
+            break;
+        }
+        OperationNumber++;
+    }
+}
