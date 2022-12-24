f7ea47c7648118534e239569e0c52f8977a262e4
Kazvin <120466779+Kazvin74@users.noreply.github.com>
2022-12-24 23:42:42 +0800

    Updated comment in the code

--- Calculators/Number Calculators/Classic Calculator.cpp
+++ Calculators/Number Calculators/Classic Calculator.cpp
@@ -3,8 +3,9 @@ Classic Calculator
 Created by Kazvin
 
 Note:
-- If any of the answers is more than 6 digits (like 1000000),
-  the answer would not be accurate. Please take note that it's not a bug.
+- If any of the answers is more than 6 digits (like 1000000) or more than
+  6 decimal places (like 0.0000001), the answer would not be accurate. Please
+  take note that it's not a bug.
 - Also, this is just a modified version of my Single Operation Calculator.
 - You can type "Ans" to make the first number or the second number equal
   to the answer in the previous equation.
