a631075cfce5405f799f02c11554a98ac34f1bfa
Kazvin <120466779+Kazvin74@users.noreply.github.com>
2023-01-22 19:59:09 +0800

    Reduced number of code lines

--- Collatz Conjecture.cpp
+++ Collatz Conjecture.cpp
@@ -1,8 +1,4 @@
-/*
-Collatz Conjecture
-Created by Kazvin
-*/
-
+// Collatz Conjecture by Kazvin
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -11,27 +7,20 @@ int main() {
     double Number, DoubleQuotient;
     int Remainder, IntQuotient, SequenceNumber = 1;
     string IncludeSequenceNumber;
-    cout << "Enter your number: ";
-    cin >> Number;
-    cout << endl << "Include sequence number? (Y or N) " << endl;
-    cin >> IncludeSequenceNumber;
+    cout << "Enter your number: "; cin >> Number;
+    cout << endl << "Include sequence number? (Y or N) " << endl; cin >> IncludeSequenceNumber;
     if (IncludeSequenceNumber != "Y" || IncludeSequenceNumber != "N") {
-        cout << endl << "Invalid Input";
-        return 0;
+        cout << endl << "Invalid Input"; return 0;
     }
     cout << endl << "Sequence: " << endl;
     while (Number > 1) {
         IntQuotient = Number / 2;
         DoubleQuotient = Number / 2;
         Remainder = Number - (IntQuotient * 2);
-        if (Remainder == 1)
-            Number = 3 * Number + 1;
-        else if (Remainder == 0)
-            Number = Number / 2;
-        if (IncludeSequenceNumber == "N")
-            cout << Number << endl;
-        else if (IncludeSequenceNumber == "Y")
-            cout << SequenceNumber << ". " << Number << endl;
+        if (Remainder == 1) Number = 3 * Number + 1;
+        else if (Remainder == 0) Number = Number / 2;
+        if (IncludeSequenceNumber == "N") cout << Number << endl;
+        else if (IncludeSequenceNumber == "Y") cout << SequenceNumber << ". " << Number << endl;
         SequenceNumber++;
     }
     cout << endl << "You are now stuck in a 4, 2, 1 loop";
--- Fibonacci Sequence.cpp
+++ Fibonacci Sequence.cpp
@@ -1,8 +1,4 @@
-/*
-Fibonacci Sequence
-Created by Kazvin
-*/
-
+// Fibonacci Sequence by Kazvin
 #include <iostream>
 #include <iomanip>
 using namespace std;
@@ -11,47 +7,34 @@ int main() {
     double FirstNumber, SecondNumber, ThirdNumber, NumbersInSequence;
     int SequenceNumber = 1;
     string IncludeSequenceNumber;
-    cout << "Initial First Number: ";
-    cin >> FirstNumber;
-    cout << "Initial Second Number: ";
-    cin >> SecondNumber;
-    cout << "Numbers in the sequence: ";
-    cin >> NumbersInSequence;
-    cout << endl << "Include sequence number? (Y or N) " << endl;
-    cin >> IncludeSequenceNumber;
+    cout << "Initial First Number: "; cin >> FirstNumber;
+    cout << "Initial Second Number: "; cin >> SecondNumber;
+    cout << "Numbers in the sequence: "; cin >> NumbersInSequence;
+    cout << endl << "Include sequence number? (Y or N) " << endl; cin >> IncludeSequenceNumber;
     if (IncludeSequenceNumber != "Y" || IncludeSequenceNumber != "N") {
-        cout << endl << "Invalid Input";
-        return 0;
+        cout << endl << "Invalid Input"; return 0;
     }
     cout << endl << "Sequence:" << endl;
     if (IncludeSequenceNumber == "N") {
         while (SequenceNumber <= NumbersInSequence) {
-            if (SequenceNumber == 1)
-                cout << fixed << setprecision(0) << FirstNumber << endl;
-            else if (SequenceNumber == 2)
-                cout << fixed << setprecision(0) << SecondNumber << endl;
+            if (SequenceNumber == 1) cout << fixed << setprecision(0) << FirstNumber << endl;
+            else if (SequenceNumber == 2) cout << fixed << setprecision(0) << SecondNumber << endl;
             else {
                 ThirdNumber = FirstNumber + SecondNumber;
                 cout << fixed << setprecision(0) << ThirdNumber << endl;
-                FirstNumber = SecondNumber;
-                SecondNumber = ThirdNumber;
-                ThirdNumber = 0;
+                FirstNumber = SecondNumber; SecondNumber = ThirdNumber; ThirdNumber = 0;
             }
             SequenceNumber++;
         }
     }
     else if (IncludeSequenceNumber == "Y") {
         while (SequenceNumber <= NumbersInSequence) {
-            if (SequenceNumber == 1)
-                cout << SequenceNumber << ". " << fixed << setprecision(0) << FirstNumber << endl;
-            else if (SequenceNumber == 2)
-                cout << SequenceNumber << ". " << fixed << setprecision(0) << SecondNumber << endl;
+            if (SequenceNumber == 1) cout << SequenceNumber << ". " << fixed << setprecision(0) << FirstNumber << endl;
+            else if (SequenceNumber == 2) cout << SequenceNumber << ". " << fixed << setprecision(0) << SecondNumber << endl;
             else {
                 ThirdNumber = FirstNumber + SecondNumber;
                 cout << SequenceNumber << ". " << fixed << setprecision(0) << ThirdNumber << endl;
-                FirstNumber = SecondNumber;
-                SecondNumber = ThirdNumber;
-                ThirdNumber = 0;
+                FirstNumber = SecondNumber; SecondNumber = ThirdNumber; ThirdNumber = 0;
             }
             SequenceNumber++;
         }
