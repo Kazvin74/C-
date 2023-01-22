b7ab37d63497845fc1eebb7faf7f022a93124e58
Kazvin <120466779+Kazvin74@users.noreply.github.com>
2023-01-22 19:54:50 +0800

    Made some changes

--- Calculators/Area Calculator.cpp
+++ Calculators/Area Calculator.cpp
@@ -1,6 +1,5 @@
 /*
-Area Calculator
-Created by Kazvin
+Area Calculator by Kazvin
 
 References:
 Area of Semicircle: https://www.easycalculation.com/area/area-semicircle-calculator.php
@@ -17,133 +16,59 @@ using namespace std;
 int main() {
     const double Pi = 3.1415926535;
     double Area;
-    cout << "Shapes:" << endl;
-    cout << "C = Circle" << endl;
-    cout << "SC = Semicircle" << endl;
-    cout << "E = Ellipse" << endl;
-    cout << "SE = Sector" << endl;
-    cout << "T = Trapezoid" << endl;
-    cout << "3 = Triangle" << endl;
-    cout << "4 = Rectangle" << endl;
-    cout << "5 = Pentagon" << endl;
-    cout << "6 = Hexagon" << endl;
-    cout << "7 = Heptagon" << endl;
-    cout << "8 = Octagon" << endl;
-    cout << "9 = Nonagon" << endl;
-    cout << "10 = Decagon" << endl << endl;
-    string Shape;
-    cout << "Shape (ex. SC): ";
-    cin >> Shape;
     int DecimalPlaces;
-    cout << "Decimal Places: ";
-    cin >> DecimalPlaces;
-    cout << endl << "Shape: ";
+    double Radius, RadiusA, RadiusB, Angle, Base, BaseA, BaseB, Height, Side;
+    string Shape;
+    cout << "Shapes:" << endl << "C = Circle" << endl << "SC = Semicircle" << endl;
+    cout << "E = Ellipse" << endl << "SE = Sector" << endl << "T = Trapezoid" << endl;
+    cout << "3 = Triangle" << endl << "4 = Rectangle" << endl << "5 = Pentagon" << endl;
+    cout << "6 = Hexagon" << endl << "7 = Heptagon" << endl << "8 = Octagon" << endl;
+    cout << "9 = Nonagon" << endl << "10 = Decagon" << endl << endl;
+    cout << "Shape (ex. SC): "; cin >> Shape;
+    cout << "Decimal Places: "; cin >> DecimalPlaces; cout << endl;
     if (Shape == "C") {
-        cout << "Circle" << endl;
-        double Radius;
-        cout << "Radius: ";
-        cin >> Radius;
+        cout << "Radius: "; cin >> Radius;
         Area = Pi * pow(Radius, 2);
     }
     else if (Shape == "SC") {
-        cout << "Semicircle" << endl;
-        double Radius;
-        cout << "Radius: ";
-        cin >> Radius;
+        cout << "Radius: "; cin >> Radius;
         Area = Pi * pow(Radius, 2) * 0.5;
     }
     else if (Shape == "E") {
-        cout << "Ellipse" << endl;
-        double RadiusA, RadiusB;
-        cout << "Radius A: ";
-        cin >> RadiusA;
-        cout << "Radius B: ";
-        cin >> RadiusB;
+        cout << "Radius A: "; cin >> RadiusA;
+        cout << "Radius B: "; cin >> RadiusB;
         Area = Pi * RadiusA * RadiusB;
     }
     else if (Shape == "SE") {
-        cout << "Sector" << endl;
-        double Angle, Radius;
-        cout << "Angle: ";
-        cin >> Angle;
-        cout << "Radius: ";
-        cin >> Radius;
+        cout << "Angle: "; cin >> Angle;
+        cout << "Radius: "; cin >> Radius;
         Area = (Angle/360.0) * Pi * pow(Radius, 2);
     }
     else if (Shape == "T") {
-        cout << "Trapezoid" << endl;
-        double BaseA, BaseB, Height;
-        cout << "Base A: ";
-        cin >> BaseA;
-        cout << "Base B: ";
-        cin >> BaseB;
-        cout << "Height: ";
-        cin >> Height;
+        cout << "Base A: "; cin >> BaseA;
+        cout << "Base B: "; cin >> BaseB;
+        cout << "Height: "; cin >> Height;
         Area = (BaseA + BaseB) * 0.5 * Height;
     }
     else if (Shape == "3") {
-        cout << "Triangle" << endl;
-        double Base, Height;
-        cout << "Base: ";
-        cin >> Base;
-        cout << "Height: ";
-        cin >> Height;
+        cout << "Base: "; cin >> Base;
+        cout << "Height: "; cin >> Height;
         Area = (Base * Height) * 0.5;
     }
     else if (Shape == "4") {
-        cout << "Rectangle" << endl;
-        double Base, Height;
-        cout << "Base: ";
-        cin >> Base;
-        cout << "Height: ";
-        cin >> Height;
+        cout << "Base: "; cin >> Base;
+        cout << "Height: "; cin >> Height;
         Area = (Base * Height);
     }
-    else if (Shape == "5") {
-        cout << "Pentagon" << endl;
-        double Side;
-        cout << "Side: ";
-        cin >> Side;
-        Area = 0.25 * (sqrt(5 * ( 5 + 2 * sqrt(5))) * pow(Side, 2));
+    else if (Shape == "5" || Shape == "6" || Shape == "7" || Shape == "8" || Shape == "9" || Shape == "10") {
+        cout << "Side: "; cin >> Side;
+        if (Shape == "5") Area = 0.25 * (sqrt(5 * ( 5 + 2 * sqrt(5))) * pow(Side, 2));
+        else if (Shape == "6") Area = (3 * sqrt(3)) * 0.5 * pow(Side, 2);
+        else if (Shape == "7") Area = 3.6339124325 * pow(Side, 2);
+        else if (Shape == "8") Area = 2 * (1 + sqrt(2)) * pow(Side, 2);
+        else if (Shape == "9") Area = 6.18182417 * pow(Side, 2);
+        else if (Shape == "10") Area = 2.5 * pow(Side, 2) * sqrt(5 + 2 * sqrt(5));
     }
-    else if (Shape == "6") {
-        cout << "Hexagon" << endl;
-        double Side;
-        cout << "Side: ";
-        cin >> Side;
-        Area = (3 * sqrt(3)) * 0.5 * pow(Side, 2);
-    }
-    else if (Shape == "7") {
-        cout << "Heptagon" << endl;
-        double Side;
-        cout << "Side: ";
-        cin >> Side;
-        Area = 3.6339124325 * pow(Side, 2);
-    }
-    else if (Shape == "8") {
-        cout << "Octagon" << endl;
-        double Side;
-        cout << "Side: ";
-        cin >> Side;
-        Area = 2 * (1 + sqrt(2)) * pow(Side, 2);
-    }
-    else if (Shape == "9") {
-        cout << "Nonagon" << endl;
-        double Side;
-        cout << "Side: ";
-        cin >> Side;
-        Area = 6.18182417 * pow(Side, 2);
-    }
-    else if (Shape == "10") {
-        cout << "Decagon" << endl;
-        double Side;
-        cout << "Side: ";
-        cin >> Side;
-        Area = 2.5 * pow(Side, 2) * sqrt(5 + 2 * sqrt(5));
-    }
-    else {
-        cout << "(Invalid Input)";
-        return 0;
-    }
+    else {cout << "Invalid Input"; return 0;}
     cout << "Area: " << fixed << setprecision(DecimalPlaces) << Area;
 }
