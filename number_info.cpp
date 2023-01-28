7396bbc850b7a3f7be2abc0cd263736c5e20d3ff
Kazvin <kuzzar2009@gmail.com>
2023-01-28 23:26:30 +0800

    v202301282326
    Added number_info.cpp

---
+++ number_info.cpp
@@ -0,0 +1,33 @@
+// Number Info by Kazvin
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main () {
+    long double Number;
+    cout << "Number: ";
+    cin >> Number;
+    cout << "Classification: ";
+    if (Number - (round(Number/1)) != 0)
+        cout << "Rational";
+    else if (Number < 0)
+        cout << "Rational, Integer";
+    else if (Number == 0)
+        cout << "Rational, Integer, Whole";
+    else if (Number > 0)
+        cout << "Rational, Integer, Whole, Counting";
+    cout << endl << "Sign: ";
+    if (Number < 0)
+        cout << "Negative";
+    else if (Number == 0)
+        cout << "Zero";
+    else if (Number > 0)
+        cout << "Positive";
+    if (Number - (round(Number/1)) == 0) {
+        cout << endl << "Parity: ";
+        if (Number - round(Number/2) == Number/2)
+            cout << "Even";
+        else if (Number - round(Number/2) != Number/2)
+            cout << "Odd";
+    }
+}
